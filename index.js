bed6516f74e33b34467258275739f789a71a72b2
Nick Sherman <ogleffeckt2@yahoo.com>
2020-10-08 05:46:47 -0400

    set up body-parser & passport, still tweaking

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 app.listen(5000, () => {
 	console.log('Listening on 5000');
