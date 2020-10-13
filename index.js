436ebc2b24de04747dd7f473a0aaae57e8784666
Nick Sherman <ogleffeckt2@yahoo.com>
2020-10-13 06:17:17 -0400

    server bones there, need to add g-auth & mongo secrets

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
+const keys = require('./config/keys');
+require('./models/User');
+
+mongoose.connect(keys.mongoURI, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true
+});
 
 const app = express();
 
