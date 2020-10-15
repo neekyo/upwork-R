8ead23427f443c410e5815a03f9e2b80b4efab25
Nick Sherman <ogleffeckt2@yahoo.com>
2020-10-15 08:47:52 -0400

    set up browser router & header boiler

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Header from './Header';
 
 const App = () => {
-	return <div>App</div>;
+	return (
+		<BrowserRouter>
+			<React.Fragment>
+				<Header />
+				<App />
+			</React.Fragment>
+		</BrowserRouter>
+	);
 };
 
 export default App;
