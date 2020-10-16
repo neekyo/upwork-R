492d7ce4051e2713156b56224864a11f791737f1
Nick Sherman <ogleffeckt2@yahoo.com>
2020-10-16 09:20:49 -0400

    header progress

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-
 import Header from './Header';
 
 const App = () => {
-	return (
+	return (	
 		<BrowserRouter>
 			<React.Fragment>
 				<Header />
-				<App />
 			</React.Fragment>
 		</BrowserRouter>
 	);
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<React.Fragment>
+			<div>
+				<Link to="/">Home</Link>
+				<a href="auth/google" className="ui green google button">
+					<div>
+						<i className="google icon" />
+						Sign in with Google
+					</div>
+				</a>
+				<a href="api/logout" className="ui red google button">
+					<div>
+						<i className="google icon" />
+						Sign Out
+					</div>
+				</a>
+			</div>
+		</React.Fragment>
+	);
 };
 
 export default Header;
